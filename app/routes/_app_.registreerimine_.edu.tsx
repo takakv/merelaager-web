b932707770c671fcb77f4af9d2bce5bab25b159d
Taka <71265675+takakv@users.noreply.github.com>
2024-01-01 11:00:45 +0200

    Fix honorifics

--- app/routes/_app_.registreerimine_.edu.tsx
+++ app/routes/_app_.registreerimine_.edu.tsx
@@ -2,13 +2,13 @@ export default function RegSuccessRoute() {
   return (
     <main className="successpage">
       <section className="success-message">
-        <h3 className="">Oleme teid lisanud reservnimekirja</h3>
+        <h3 className="">Oleme Teid lisanud reservnimekirja</h3>
         <p>
-          Saatsime teie meilile teate registreerimise lisainfo ja
+          Saatsime Teie meilile teate registreerimise lisainfo ja
           reservnimekirja kandmise kohta.
         </p>
         <p className="disclaimer">
-          Kui te pole kümne minuti jooksul meilile teadet saanud, siis palun
+          Kui Te pole kümne minuti jooksul meilile teadet saanud, siis palun
           helistage / kirjutage vahetuse juhatajale. Soovitame vaadata ka
           rämpsposti.
         </p>
