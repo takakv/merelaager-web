bc96ef93dce570c54c8351bb5a756175af57b2cf
Taka <71265675+takakv@users.noreply.github.com>
2024-12-28 17:07:02 +0100

    Update prices

--- app/routes/info/maksmine.tsx
+++ app/routes/info/maksmine.tsx
@@ -20,15 +20,15 @@ const PriceInfo = () => {
           <div className="c-payment-block">
             <h4 className="c-payment-block__title">12päevane vahetus</h4>
             <ul className="c-payment-block__content">
-              <li>uuele tulijale: 340 €</li>
-              <li>vanale olijale: 320 €</li>
+              <li>uuele tulijale: 350 €</li>
+              <li>vanale olijale: 330 €</li>
             </ul>
           </div>
           <div className="c-payment-block">
-            <h4 className="c-payment-block__title">11päevane vahetus</h4>
+            <h4 className="c-payment-block__title">10päevane vahetus</h4>
             <ul className="c-payment-block__content">
-              <li>uuele tulijale: 320 €</li>
-              <li>vanale olijale: 300 €</li>
+              <li>uuele tulijale: 290 €</li>
+              <li>vanale olijale: 280 €</li>
             </ul>
           </div>
           <div className="c-payment-block">
