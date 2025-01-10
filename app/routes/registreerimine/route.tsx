b205ae20f53974036832f3e19851371a15752a5c
Taka <71265675+takakv@users.noreply.github.com>
2025-01-10 21:37:44 +0100

    Do not display reserve slots in counters

--- app/routes/registreerimine/route.tsx
+++ app/routes/registreerimine/route.tsx
@@ -65,6 +65,26 @@ export const loader = async () => {
     remainingSlots[registration.shiftNr][registration.children.gender] -= 1;
   });
 
+  // Display the max number of slots, disregarding reserve, in case nobody
+  // has been registered yet. This prevents confusion as to why registration
+  // counters are lower than expected at the start of the registration period.
+  shifts.forEach((shift) => {
+    const maxFreeBoySlots = shift.boySlots - shift.boySlotsReserve;
+    const maxFreeGirlSlots = shift.girlSlots - shift.girlSlotsReserve;
+    if (
+      remainingSlots[shift.id].M >= 0 &&
+      remainingSlots[shift.id].M === maxFreeBoySlots
+    ) {
+      remainingSlots[shift.id].M = shift.boySlots;
+    }
+    if (
+      remainingSlots[shift.id].F >= 0 &&
+      remainingSlots[shift.id].F === maxFreeGirlSlots
+    ) {
+      remainingSlots[shift.id].F = shift.girlSlots;
+    }
+  });
+
   return {
     shifts,
     remainingSlots,
